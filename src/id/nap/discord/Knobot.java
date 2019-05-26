f4f62a229ea78e729b748ca19bb3a94322061d2e
nap <nap.tyo@gmail.com>
2019-05-26 12:38:25 +0700

    Check if DISCORD_TOKEN is not null in the system environment

--- src/id/nap/discord/Knobot.java
+++ src/id/nap/discord/Knobot.java
@@ -8,13 +8,30 @@ import net.dv8tion.jda.core.JDABuilder;
 
 public class Knobot {
 	
+	public static final int NO_TOKEN_FOUND = 500;
+	
 	public static void main(String[] args) throws LoginException {
 		String token = "";
+		
+		try {
+			token = getToken();
+		} catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+			System.exit(NO_TOKEN_FOUND);
+		}
+		
 		JDABuilder builder = new JDABuilder(AccountType.BOT).setToken(token);
 		
 		builder.addEventListener(new Ping());
 		
 		builder.build();
 	}
-
+	
+	private static String getToken() {
+		if (System.getenv("DISCORD_TOKEN") == null) {
+			throw new NullPointerException("No DISCORD_TOKEN found in system environment.");
+		}
+		
+		return System.getenv("DISCORD_TOKEN");
+	}
 }
