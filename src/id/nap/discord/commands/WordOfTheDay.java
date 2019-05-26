2a9a7196ca3f785729cba412cd85adba89a8e70d
nap <nap.tyo@gmail.com>
2019-05-26 16:45:03 +0700

    wotd class

---
+++ src/id/nap/discord/commands/WordOfTheDay.java
@@ -0,0 +1,16 @@
+package id.nap.discord.commands;
+
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class WordOfTheDay extends Command {
+
+	@Override
+	public void onCommand(MessageReceivedEvent event, String[] args) {
+		
+	}
+
+	@Override
+	public String getCommand() {
+		return "!wotd";
+	}
+}
