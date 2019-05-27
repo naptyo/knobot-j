d98656182fb39bd779915f254a6713986463023e
afian <afian@adrena.id>
2019-05-27 17:43:01 +0700

    refactor logging for ping

--- src/id/nap/discord/commands/Ping.java
+++ src/id/nap/discord/commands/Ping.java
@@ -1,28 +1,28 @@
 package id.nap.discord.commands;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
 public class Ping extends Command {
-	private static final Logger logger = LogManager.getLogger(Ping.class);
 	
 	@Override
 	public void onCommand(MessageReceivedEvent event, String[] args) {
-		logger.info(
-				event.getAuthor().getName() 
-				+ " sent " 
-				+ event.getMessage().getContentDisplay()
-				+ " in "
-				+ event.getChannel().getName());
-		
+		logCommand(event, args);
 		sendMessage(event, "Pong!");
 	}
 
 	@Override
 	public String getCommand() {
 		return "!ping";
+	}
+
+	@Override
+	public void logCommand(MessageReceivedEvent event, String[] args) {
+		LOGGER.info(
+				event.getAuthor().getName()
+				+ " sent "
+				+ event.getMessage().getContentDisplay()
+				+ " in "
+				+ event.getChannel().getName());
 	}
 	
 }
