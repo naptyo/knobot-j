cb523923a309b70859b640cbe113a6adf210cf87
afian <afian@adrena.id>
2019-05-30 08:04:10 +0700

    add variables to meta to accomodate errors response

--- src/main/java/id/nap/discord/model/calendarific/Meta.java
+++ src/main/java/id/nap/discord/model/calendarific/Meta.java
@@ -2,14 +2,18 @@ package id.nap.discord.model.calendarific;
 
 public class Meta {
 	private int code;
+	private String error_type;
+	private String error_detail;
 	
 	public Meta() {
 		super();
 	}
 
-	public Meta(int code) {
+	public Meta(int code, String error_type, String error_detail) {
 		super();
 		this.code = code;
+		this.error_type = error_type;
+		this.error_detail = error_detail;
 	}
 
 	public int getCode() {
@@ -18,5 +22,21 @@ public class Meta {
 
 	public void setCode(int code) {
 		this.code = code;
+	}
+
+	public String getError_type() {
+		return error_type;
+	}
+
+	public void setError_type(String error_type) {
+		this.error_type = error_type;
+	}
+
+	public String getError_detail() {
+		return error_detail;
+	}
+
+	public void setError_detail(String error_detail) {
+		this.error_detail = error_detail;
 	}
 }
