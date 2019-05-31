c2703b06dc7da7114b4e1fb9b0c2f5d39c372a19
nap <nap.tyo@gmail.com>
2019-06-01 00:53:48 +0700

    add shut down command

--- src/main/java/id/nap/discord/Knobot.java
+++ src/main/java/id/nap/discord/Knobot.java
@@ -4,6 +4,7 @@ import javax.security.auth.login.LoginException;
 
 import id.nap.discord.commands.HolidayCommand;
 import id.nap.discord.commands.PingCommand;
+import id.nap.discord.commands.ShutDownCommand;
 import id.nap.discord.commands.WOTDCommand;
 import id.nap.discord.model.Config;
 import net.dv8tion.jda.core.AccountType;
@@ -11,7 +12,7 @@ import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.Game;
 
 public class Knobot {
-	
+	public static final int SHUTDOWN = 200;
 	public static final int NO_CONFIG_FOUND = 501;
 	public static final int IO_ERROR = 502;
 	public static final int BAD_UNICODE_ESCAPE = 503;
@@ -24,6 +25,7 @@ public class Knobot {
 		builder.addEventListener(new PingCommand());
 		builder.addEventListener(new WOTDCommand());
 		builder.addEventListener(new HolidayCommand());
+		builder.addEventListener(new ShutDownCommand());
 		
 		builder.setGame(Game.playing("in 127.0.0.1"));
 		
---
+++ src/main/java/id/nap/discord/commands/ShutDownCommand.java
@@ -0,0 +1,24 @@
+package id.nap.discord.commands;
+
+import id.nap.discord.Knobot;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class ShutDownCommand extends Command {
+	
+	private final static String USER_ID = "170384700433629184";
+	
+	@Override
+	public void onCommand(MessageReceivedEvent event, String[] args) {
+		if (event.getAuthor().getId().equals(USER_ID)) {
+			sendMessage(event, "Shutting down...");
+			System.out.println("Bot has shut down.");
+			System.exit(Knobot.SHUTDOWN);
+		}
+	}
+
+	@Override
+	public String getCommand() {
+		return "!shutdown";
+	}
+	
+}
